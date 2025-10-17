059edcc11a796be476f01279885ee209e406e769
tailv <levantai@.com>
2025-10-17 12:37:18 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+setInterval(function upTime() {
+  const now = new Date();
+  console.log(now);
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const time = `${hours} : ${minutes} : ${seconds}`;
+  const layGiatri = document.getElementById("clock");
+  layGiatri.textContent = time;
+}, 1000);
